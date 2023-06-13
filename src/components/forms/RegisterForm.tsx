c05cf1c65211c9100450d0563a1aa841c13f16f1
akopton <kopton.olek@gmail.com>
2023-06-14 00:31:46 +0200

    register form structure

--- src/components/forms/RegisterForm.tsx
+++ src/components/forms/RegisterForm.tsx
@@ -1,17 +1,50 @@
 "use client"
 
-import { useState } from "react"
+import React, { useState } from "react"
+import { CustomInput } from "./CustomInput"
 
 export const RegisterForm = () => {
-  const [email, setEmail] = useState<string>("")
   const [displayName, setDisplayName] = useState<string>("")
+  const [email, setEmail] = useState<string>("")
   const [password, setPassword] = useState<string>("")
 
+  const handleName = (e: React.FormEvent<HTMLInputElement>) => {
+    setDisplayName(e.currentTarget.value)
+  }
+  const handleEmail = (e: React.FormEvent<HTMLInputElement>) => {
+    setEmail(e.currentTarget.value)
+  }
+  const handlePassword = (e: React.FormEvent<HTMLInputElement>) => {
+    setPassword(e.currentTarget.value)
+  }
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+  }
+
   return (
-    <form className="flex flex-col gap-3">
-      <input type="text" />
-      <input type="text" />
-      <input type="text" />
+    <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
+      <CustomInput
+        id="name"
+        name="name"
+        type="text"
+        value={displayName}
+        onChange={handleName}
+      />
+      <CustomInput
+        id="email"
+        name="email"
+        type="email"
+        value={email}
+        onChange={handleEmail}
+      />
+      <CustomInput
+        id="password"
+        name="password"
+        type="password"
+        value={password}
+        onChange={handlePassword}
+      />
       <input type="submit" value="Send" />
     </form>
   )
